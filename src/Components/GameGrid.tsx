d46a5d269714054530e3350506a612923ca717e8
Akshat Saxena <akshatsaxena799@gmail.com>
2025-03-20 20:34:44 +0530

    Building game cards

--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import apiClient from "../Services/api-services";
-import { Text } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
 
 const GameGrid = ()=>{
 
@@ -11,13 +12,10 @@ const GameGrid = ()=>{
   return (
     <>
       {error && <Text color="red.500">{error}</Text>}
-      <ul>
-        {games && games.length > 0 ? (
-          games.map((game) => <li key={game.id}>{game.name}</li>)
-        ) : (
-          <li>No games found</li>
-        )}
-      </ul>
+      <SimpleGrid columns={{sm :1,md:2,lg:3}} spacing={10} padding='10px'>
+       {games.map((game) => <GameCard key={game.id} game={game}/>)}
+    
+      </SimpleGrid>
     </>
   );
 };
